42ecb4410729ff2bac0578bf5bfc08cb3947d0da
Goose <49313915+Goose-Student@users.noreply.github.com>
2024-05-02 12:08:57 +0300

    Update extend keyy 4lb

---
+++ 4LB/source/common/io.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class Type
+{
+private:
+    string _type;
+
+public:
+    // Конструктор
+    Type(string _type)
+    {
+        this->_type = _type;
+    }
+
+    // Геттер
+    operator string() const
+    {
+        return _type;
+    }
+
+    // Перегрузка оператора == для сравнения объектов
+    bool operator==(const Type &type) const
+    {
+        return _type == string(type);
+    }
+};
+
+// Класс с типами данных
+class Types
+{
+public:
+    static Type HEX;
+    static Type BIN;
+    static Type DEC;
+};
+
+Type Types::HEX = Type("HEX");
+Type Types::BIN = Type("BIN");
+Type Types::DEC = Type("DEC");
+
+// Функции вывода
+template <typename T>
+void printBytes(T value, const Type &type = Types::DEC)
+{
+    if (type == Types::HEX)
+        cout << "0x" << setfill('0') << setw(sizeof(T) * 2) << uppercase << hex << uint64_t(value) << " ";
+    else if (type == Types::BIN)
+    {
+        cout << "0b";
+        for (int i = sizeof(T) * 8 - 1; i >= 0; i--)
+            cout << ((value >> i) & 1);
+        cout << " ";
+    }
+    else
+        cout << dec << uint64_t(value) << " ";
+}
+
+template <typename T>
+void printBytes(vector<T> values, const Type &type = Types::DEC)
+{
+    int count = 0;
+    for (const T &value : values)
+    {
+        printBytes(value, type);
+        count += 1;
+        if (count % 16 == 0)
+            cout << endl;
+    }
+}
+
+// Функции записи
+template <typename T>
+void writeBytes(string path, vector<T> values)
+{
+    ofstream file(path, ios::binary);
+    for (T value : values)
+    {
+        file.write(reinterpret_cast<const char *>(&value), sizeof(T));
+    }
+    file.close();
+}
+
+vector<uint8_t> readBytes(const string &path)
+{
+    ifstream file(path, ios::binary);
+    if (!file)
+        throw runtime_error(path);
+
+    vector<uint8_t> bytes((istreambuf_iterator<char>(file)),
+                          istreambuf_iterator<char>());
+    return bytes;
+}
--- 4LB/source/main.cpp
+++ 4LB/source/main.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "./common/io.cpp"
 
 using namespace std;
 
-uint64_t t(uint64_t input)
-{
-    return ~input;
-}
+vector<uint8_t> S[8] = {{12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1},
+                        {6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15},
+                        {11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0},
+                        {12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11},
+                        {7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12},
+                        {5, 13, 15, 6, 9, 2, 12, 10, 11, 7, 8, 1, 4, 3, 14, 0},
+                        {8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7},
+                        {1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2}};
 
-uint64_t g(vector<uint8_t> key, uint64_t input)
+class RoundKey
 {
-    return ~input;
-}
+private:
+    vector<uint8_t> bytes;
 
-vector<uint8_t> keyExpansion(vector<uint8_t> key)
+public:
+    RoundKey(string path)
+    {
+        bytes = readBytes(path);
+        if (bytes.size() != 7)
+            throw range_error("The key must be 56 bits in size");
+    };
+
+    uint32_t operator[](int index) const
+    {
+        if (index < 0 || index > 31)
+            throw range_error("The index must be in range [0, 32)");
+
+        if (index > 23)
+            index = 31 - index;
+            
+        uint32_t nkey = 0;
+        for (int i = 0; i < 4; i++)
+            nkey |= (bytes[(index * 4 + i) % 7] << (3 - i) * 8);
+        return nkey;
+    }
+};
+
+int main()
 {
-    if (key.size() != 7)
-        throw range_error("The key must be 56 bits in size");
+    vector<uint8_t> key = readBytes("key.key");
+    printBytes(key, Types::HEX);
 
-    vector<uint8_t> extendedKey;
-    for (int i = 0; i < 32; ++i)
-        extendedKey.push_back(key[i % 7]);
-    return extendedKey;
+    cout << endl;
+
+    RoundKey round_key("key.key");
+    for (int i = 0; i < 32; i++)
+    {
+        if (i % 8 == 0)
+            cout << endl;
+        printBytes(round_key[i], Types::HEX);
+        cout << endl;
+    }
+
+    system("pause");
+    return 0;
 }
